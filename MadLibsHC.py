f29b30e52ef56488a0dd2127512a86e8a7ceb596
KitsuneNoctus <theceintiant@gmail.com>
2019-08-29 23:24:53 -0700

    Got the create story which takes in user input to work

--- MadLibsHC.py
+++ MadLibsHC.py
@@ -3,19 +3,30 @@
 # August 28, 2019
 # Purpose: Creating a MadLinbs game in Python
 
-inputWords = list()
-story1Words = ["Noun","Adj","Verb"]
+inputWords = ["None"]
+story1Words = ("Noun","Adj","Verb")
 
 # def takeInput(list):
 #     #Taking in the words from user and assigning it to an array for use later
 #     return 0
 
-def createStory(list):
-    for i in range(0,len(list)):
-        wordChoice = input("Enter a " + list[i] + ": ")
-        #print("Give a "+)
-        list[i] = wordChoice
-    print(list)
+def createStory(listOfWords):
+    storedList = list(listOfWords)
+    for i in range(0, len(listOfWords)-1):
+        storedList[i] = input("Enter a " + listOfWords[i] + ": ")
+        # inputWords[i].append(wordChoice)
+    #print(storedList)
+
+
+# def printStory(choice, wordsGiven):
+#     story1 = ["Hi There ","You look ","Keep on the "]
+#     if choice == 1:
+#         storyChoice = story1
+#     else:
+#         print("Nothing Selected")
+#
+#     for i in range(0, len(storyChoice)):
+#         print(storyChoice[i]+ wordsGiven[i])
 
 
 
@@ -27,13 +38,16 @@ def MadGame():
         print("Choose A Story: ")
         print(" 1) Egg")
         print("Q = Quit")
+        inputWords.clear()
         choice = input("Which Story?: ")
 
         if choice == "1":
             createStory(story1Words)
+            # printStory(choice, inputWords)
         elif choice == "Q":
             on = False
         else:
             print("Sorry, That isn't an option")
 
 MadGame()
+print(story1Words)
