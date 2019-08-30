e7bdb1a1bb8b60e9d9a487695b23e9e6401331ea
KitsuneNoctus <theceintiant@gmail.com>
2019-08-29 19:24:49 -0700

    Created start of Game

--- MadLibsHC.py
+++ MadLibsHC.py
@@ -1,5 +1,39 @@
 # HenryCalderonMadLibs
 # IDE/OS: Atom/ MAC OS
 # August 28, 2019
-# Purpose:
-print("Madlibs! The Creative Story Game")
+# Purpose: Creating a MadLinbs game in Python
+
+inputWords = list()
+story1Words = ["Noun","Adj","Verb"]
+
+def takeInput(list):
+    #Taking in the words from user and assigning it to an array for use later
+    return 0
+
+#Stuff
+def chooseStory():
+    if choice == 1:
+        return 0
+    else:
+        print("Try Again")
+
+def createStory():
+    return 0
+
+
+
+def MadGame():
+    on = True
+    while on:
+        print("Madlibs! The Creative Story Game")
+        print("--------------------------------")
+        print("Choose A Story: ")
+        print(" 1) A Man of Taste")
+        print("Q = Quit")
+        choice = input("Which Story?: ")
+        if choice == 1:
+            createStory()
+        elif choice == Q:
+            on = False
+        else:
+            print("Sorry, That isn't an option.")
